dc21ab1999301957dfc32f0b03ede1dd2182b5e6
9dog-9dog <81842500+9dog-9dog@users.noreply.github.com>
2022-03-19 02:24:50 -0700

    Increase MAX_CHALLENGES to 7

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -3,7 +3,7 @@ import { solution } from '../lib/words'
 // NOTE(9dog): The answer can be either 5 or 6 characters.
 export const MIN_WORD_LENGTH = 5
 export const MAX_WORD_LENGTH = 6
-export const MAX_CHALLENGES = 6
+export const MAX_CHALLENGES = 7
 export const ALERT_TIME_MS = 2000
 export const REVEAL_TIME_MS = 350
 export const GAME_LOST_INFO_DELAY = (MAX_WORD_LENGTH + 1) * REVEAL_TIME_MS
