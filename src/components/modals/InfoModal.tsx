ff3a57d6cba5906725404633eb6a155bdf0ba8d4
9dog-9dog <81842500+9dog-9dog@users.noreply.github.com>
2022-03-18 21:54:11 -0700

    fix: Update game title and some readme texts

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -13,59 +13,69 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         Guess the word in 6 tries. After each guess, the color of the tiles will
         change to show how close your guess was to the word.
       </p>
-
       <div className="flex justify-center mb-1 mt-4">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="D"
           status="correct"
-        />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="L"
-          status="present"
         />
         <Cell value="O" />
         <Cell value="T" />
+        <Cell value="O" />
+        <Cell value="L" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter D is in the word and in the correct spot.
       </p>
-
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
+        <Cell value="S" />
+        <Cell value="L" />
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="E"
+          status="present"
+        />
         <Cell value="E" />
+        <Cell value="P" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter E is in the word but in the wrong spot.
       </p>
-
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Also, there is only one E in the word.
+      </p>
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="D" />
+        <Cell value="E" />
+        <Cell value="G" />
+        <Cell value="E" />
+        <Cell isRevealing={true} isCompleted={true} value="N" status="absent" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter N is not in the word in any spot.
+      </p>
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+        This is an open source version of Wordle, tailored for a Twitch streamer{' '}
         <a
-          href="https://github.com/cwackerfuss/react-wordle"
+          href="https://twitch.tv/dotolkim"
+          target="_blank"
+          rel="noreferrer"
+          className="underline font-bold"
+        >
+          Dotol Kim
+        </a>
+      </p>
+      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
+        <a
+          href="https://github.com/9dog-9dog/toldle"
+          target="_blank"
+          rel="noreferrer"
           className="underline font-bold"
         >
           check out the code here
-        </a>{' '}
+        </a>
       </p>
     </BaseModal>
   )
